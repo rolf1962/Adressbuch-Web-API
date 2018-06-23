a2a9e59e6e45d621cbf27bf9103a12d107807c80
MAMAPAPA\Rolf <jansen.rolf@t-online.de>
2018-06-23 02:00:16 +0200

    Database seed, setting connectionstring to localhost and intergrated security

--- Adressbuch.DbModel/AdressbuchDbContext.cs
+++ Adressbuch.DbModel/AdressbuchDbContext.cs
@@ -12,6 +12,24 @@ namespace Adressbuch.Server.DbModel
         public AdressbuchDbContext(string connectionString) : base(connectionString)
         {
             Configuration.LazyLoadingEnabled = false;
+            this.Database.CreateIfNotExists();
+
+            if (Personen.Count() == 0)
+            {
+                InitializeData();
+            }
+        }
+
+        private void InitializeData()
+        {
+            Person rolf = new Person() { Id = Guid.NewGuid(), Name = "Jansen", Vorname = "Rolf", Geburtsdatum = new DateTime(1962, 2, 24), Created = DateTime.Now, Modified = DateTime.Now, CreatedBy = "Rolf", ModifiedBy = "Rolf" };
+            Person susanne = new Person() { Id = Guid.NewGuid(), Name = "Weitzel-Jansen", Vorname = "Susanne", Geburtsdatum = new DateTime(1966, 3, 1), Created = DateTime.Now, Modified = DateTime.Now, CreatedBy = "Rolf", ModifiedBy = "Rolf" };
+            Person max = new Person() { Id = Guid.NewGuid(), Name = "Jansen", Vorname = "Maximilian", Geburtsdatum = new DateTime(2000, 4, 8), Created = DateTime.Now, Modified = DateTime.Now, CreatedBy = "Rolf", ModifiedBy = "Rolf" };
+            Person alex = new Person() { Id = Guid.NewGuid(), Name = "Jansen", Vorname = "Alexander", Geburtsdatum = new DateTime(2007, 9, 7), Created = DateTime.Now, Modified = DateTime.Now, CreatedBy = "Rolf", ModifiedBy = "Rolf" };
+
+            Personen.AddRange(new Person[] { rolf, susanne, max, alex });
+
+            SaveChanges();
         }
 
         public DbSet<Person> Personen { get; set; }
